5294329be06db0fa50189f017a05a5051112b673
rajan <rajyotiroshan@gmail.com>
2023-01-02 19:41:28 +0530

    7_hooks

---
+++ 7_hooks/src/App.js
@@ -0,0 +1,17 @@
+
+//import InputComponent from "./components/first-app/InputComponent";
+import RegisterForm from "./components/first-app/RegisterForm";
+import { TextInputWithFocusButton } from "./components/first-app/UseRef";
+
+function App() {
+  return (
+    //<InputComponent/>
+    <>
+    <RegisterForm/>
+    <hr/>
+    <TextInputWithFocusButton/>
+    </>
+  );
+}
+
+export default App;
---
+++ 7_hooks/src/components/first-app/RegisterForm.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+export default function RegisterForm() {
+  const [form, setForm] = useState({
+    firstName: "Rajan",
+    lastName: "kumar",
+    email: "lukeJones@sculpture.com",
+  }); // form var is initialize with state obje: {firstName: '', lastName: "", email :""}
+  return (
+    <>
+      <label>
+        First name:
+        <input
+          value={form.firstName}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              firstName: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          value={form.lastName}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              lastName: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          value={form.email}
+          onChange={(e) => {
+            setForm({
+              ...form,
+              email: e.target.value,
+            });
+          }}
+        />
+      </label>
+      <p>
+        {form.firstName} {form.lastName} ({form.email})
+      </p>
+    </>
+  );
+}
