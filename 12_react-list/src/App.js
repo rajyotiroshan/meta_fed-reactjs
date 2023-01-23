ce7d6a3c00af85c708a3a1ff917600f26bc2f492
rajan <rajyotiroshan@gmail.com>
2023-01-23 09:44:41 +0530

    list rendering ex#1

--- 12_react-list/src/App.js
+++ 12_react-list/src/App.js
@@ -1,9 +1,47 @@
 import "./App.css";
+import { useState } from "react";
+
+const ToDo = (props) => (
+  <tr>
+    <td>
+      <label>{props.id}</label>
+    </td>
+    <td>
+      <input />
+    </td>
+    <td>
+      <label>{props.createdAt}</label>
+    </td>
+  </tr>
+);
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: "todo1",
+      createdAt: "18:00",
+    },
+    {
+      id: "todos2",
+      createdAt: "20:30",
+    },
+  ]);
+
+  const reverseOrder = () => {
+    setTodos([...todos].reverse());
+  };
+
   return (
     <div>
       <h1>Handling List in React</h1>
+      <button onClick={reverseOrder}>Reverse</button>
+      <table>
+        <tbody>
+          {todos.map((todo, index) => (
+            <ToDo key={index} id={todo.id} createdAt={todo.createdAt} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
