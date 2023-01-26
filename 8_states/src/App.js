97ade63f6fd31786c9072720c179601fcb77323c
rajan <rajyotiroshan@gmail.com>
2023-01-26 22:27:24 +0530

    useState

--- 8_states/src/App.js
+++ 8_states/src/App.js
@@ -29,7 +29,7 @@ function App(){
 export default App; */
 
 //Prop drilling
-function Main(props) {
+/* function Main(props) {
   return <Header msg={props.msg} />;
 }
 
@@ -64,6 +64,70 @@ function App() {
   return (
     <Main msg="I passed through the Header and the Wrapper and I reached the Button component" />
   );
+} */
+
+//Example useState
+import React from "react";
+function GoalForm(props) {
+  const [formData, setFormData] = React.useState({ goal: "", by: "" });
+
+  function changeHandler(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    props.onAdd(formData);
+    setFormData({ goal: "", by: "" });
+  }
+
+  return (
+    <>
+      <h1>My Little Lemon Goals</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="goal"
+          placeholder="Goal"
+          value={formData.goal}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          name="by"
+          placeholder="By..."
+          value={formData.by}
+          onChange={changeHandler}
+        />
+      </form>
+    </>
+  );
+}
+function ListOfGoals(props) {
+  return (
+    <ul>
+      {props.allGoals.map((g) => {
+        <li key={g.goal}>
+          <span>
+            My goal is to {g.goal}, by {g.by}
+          </span>
+        </li>;
+      })}
+    </ul>
+  );
 }
 
+function App() {
+  const [allGoals, updateAllGoals] = React.useState([]);
+
+  function addGoal(goal) {
+    updateAllGoals([...allGoals, goal]);
+  }
+  return (
+    <div className="App">
+      <GoalForm onAdd={addGoal} />
+      <ListOfGoals allGoals={allGoals} />
+    </div>
+  );
+}
 export default App;
